38f117829eef8b9c987b4a5efc98ad44df488334
ChangZhuo Chen (陳昌倬) <czchen@gmail.com>
2013-09-02 00:03:46 +0800

    Implement has_commit & get_commit

--- chewingwrapper/include/chewingwrapper.hpp
+++ chewingwrapper/include/chewingwrapper.hpp
@@ -1,13 +1,20 @@
 #pragma once
 
+#include <memory>
+
 #include <chewing.h>
 
 class ChewingWrapper {
 public:
     ChewingWrapper();
     ~ChewingWrapper();
+
+    bool has_commit();
+    std::unique_ptr<wchar_t> get_commit();
+
 private:
-    ChewingWrapper(const ChewingWrapper&) = delete;
-    ChewingWrapper& operator=(const ChewingWrapper&) = delete;
+    ChewingWrapper(const ChewingWrapper&);
+    ChewingWrapper& operator=(const ChewingWrapper&);
+    std::unique_ptr<wchar_t> convert_utf8_to_utf16(std::unique_ptr<char>&& input);
     ChewingContext *m_ctx;
 };
--- chewingwrapper/src/chewingwrapper.cpp
+++ chewingwrapper/src/chewingwrapper.cpp
@@ -1,6 +1,9 @@
 #include <chewingwrapper.hpp>
 
+#include <new>
 #include <stdexcept>
+
+#include <windows.h>
 
 ChewingWrapper::ChewingWrapper()
 :m_ctx(0)
@@ -14,4 +17,30 @@ ChewingWrapper::~ChewingWrapper()
 {
     chewing_delete(m_ctx);
     m_ctx = 0;
+}
+
+bool ChewingWrapper::has_commit()
+{
+    return !!chewing_commit_Check(m_ctx);
+}
+
+std::unique_ptr<wchar_t> ChewingWrapper::get_commit()
+{
+    // FIXME: Use chewing_free
+    std::unique_ptr<char> utf8_string(chewing_commit_String(m_ctx));
+    if (!utf8_string.get())
+        throw std::bad_alloc();
+    return convert_utf8_to_utf16(std::move(utf8_string));
+}
+
+std::unique_ptr<wchar_t> ChewingWrapper::convert_utf8_to_utf16(std::unique_ptr<char>&& input)
+{
+    int len = MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, input.get(), -1, 0, 0);
+    if (len == 0)
+        throw std::runtime_error("MultiByteToWideChar returns 0");
+
+    std::unique_ptr<wchar_t> output(new wchar_t[len]);
+
+    MultiByteToWideChar(CP_UTF8, MB_ERR_INVALID_CHARS, input.get(), -1, output.get(), len);
+    return output;
 }

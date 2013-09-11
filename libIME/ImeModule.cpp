faed720e899964e39dcfbd224b58c2fba7911a45
Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
2013-09-11 21:32:13 +0800

    Add category registration required for Windows 8 support.

--- libIME/ImeModule.cpp
+++ libIME/ImeModule.cpp
@@ -110,6 +110,22 @@ HRESULT ImeModule::registerServer(wchar_t* name, const GUID& profileGuid, LANGID
 			//if(categoryMgr->RegisterCategory(textServiceClsid_, GUID_TFCAT_DISPLAYATTRIBUTEPROVIDER, textServiceClsid) != S_OK) {
 			//	result = E_FAIL;
 			//}
+
+			// for Windows 8 store app support
+			// TODO: according to a exhaustive Google search, I found that
+			// TF_IPP_CAPS_IMMERSIVESUPPORT is required to make the IME work with Windows 8.
+			// http://social.msdn.microsoft.com/Forums/windowsapps/en-US/4c422cf1-ceb4-413b-8a7c-6881946a4c63/how-to-set-a-flag-indicating-tsf-components-compatibility
+			// Quote from the page: "To indicate that your IME is compatible with Windows Store apps, call RegisterCategory with GUID_TFCAT_TIPCAP_IMMERSIVESUPPORT."
+
+			// declare supporting immersive mode
+			if(categoryMgr->RegisterCategory(textServiceClsid_, GUID_TFCAT_TIPCAP_IMMERSIVESUPPORT, textServiceClsid_) != S_OK) {
+				result = E_FAIL;
+			}
+
+			// declare compatibility with Windows 8 system tray
+			if(categoryMgr->RegisterCategory(textServiceClsid_, GUID_TFCAT_TIPCAP_SYSTRAYSUPPORT, textServiceClsid_) != S_OK) {
+				result = E_FAIL;
+			}
 			categoryMgr->Release();
 		}
 	}

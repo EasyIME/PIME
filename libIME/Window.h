06601aa3452a7f40b0a018a1b7e5bf022a4bc1b6
Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
2013-09-14 00:15:50 +0800

    Add missing copyright notice and LGPL declaration in file headers.

--- libIME/Window.h
+++ libIME/Window.h
@@ -1,6 +1,21 @@
-// Window.h: interface for the Window class.
 //
-//////////////////////////////////////////////////////////////////////
+//	Copyright (C) 2013 Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
+//
+//	This library is free software; you can redistribute it and/or
+//	modify it under the terms of the GNU Library General Public
+//	License as published by the Free Software Foundation; either
+//	version 2 of the License, or (at your option) any later version.
+//
+//	This library is distributed in the hope that it will be useful,
+//	but WITHOUT ANY WARRANTY; without even the implied warranty of
+//	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//	Library General Public License for more details.
+//
+//	You should have received a copy of the GNU Library General Public
+//	License along with this library; if not, write to the
+//	Free Software Foundation, Inc., 51 Franklin St, Fifth Floor,
+//	Boston, MA  02110-1301, USA.
+//
 
 #if !defined(AFX_WINDOW_H__86D89A4E_5040_4FF8_B991_0C7D6502119D__INCLUDED_)
 #define AFX_WINDOW_H__86D89A4E_5040_4FF8_B991_0C7D6502119D__INCLUDED_

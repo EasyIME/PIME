3497a920d8bf84a853ce889e56e3c42b445b5711
Hong Jen Yee (PCMan) <pcman.tw@gmail.com>
2016-02-01 18:24:00 +0800

    Replace all win32 python code with ctypes + a hand-written dll (libpipe).

---
+++ libpipe/libpipe.cpp
@@ -0,0 +1,154 @@
+#include <windows.h>
+#include <Lmcons.h> // for UNLEN
+#include <Winnt.h> // for security attributes constants
+#include <aclapi.h> // for ACL
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+static SECURITY_DESCRIPTOR g_securittyDescriptor = {0};
+static SECURITY_ATTRIBUTES g_securityAttributes = { 0 };
+static PACL g_acl = NULL;
+
+extern "C" {
+
+static void init() {
+	// http://msdn.microsoft.com/en-us/library/windows/desktop/hh448449(v=vs.85).aspx
+	// define new Win 8 app related constants
+	EXPLICIT_ACCESS explicit_accesses[2];
+	memset(&explicit_accesses, 0, sizeof(explicit_accesses));
+	// Create a well-known SID for the Everyone group.
+	// FIXME: we should limit the access to current user only
+	// See this article for details: https://msdn.microsoft.com/en-us/library/windows/desktop/hh448493(v=vs.85).aspx
+
+	PSID everyoneSID = NULL;
+	SID_IDENTIFIER_AUTHORITY worldSidAuthority = SECURITY_WORLD_SID_AUTHORITY;
+	AllocateAndInitializeSid(&worldSidAuthority, 1,
+		SECURITY_WORLD_RID, 0, 0, 0, 0, 0, 0, 0, &everyoneSID);
+
+	// https://services.land.vic.gov.au/ArcGIS10.1/edESRIArcGIS10_01_01_3143/Python/pywin32/PLATLIB/win32/Demos/security/explicit_entries.py
+
+	EXPLICIT_ACCESS& ea = explicit_accesses[0];
+	ea.grfAccessPermissions = GENERIC_ALL;
+	ea.grfAccessMode = SET_ACCESS;
+	ea.grfInheritance = SUB_CONTAINERS_AND_OBJECTS_INHERIT;
+	ea.Trustee.pMultipleTrustee = NULL;
+	ea.Trustee.MultipleTrusteeOperation = NO_MULTIPLE_TRUSTEE;
+	ea.Trustee.TrusteeForm = TRUSTEE_IS_SID;
+	ea.Trustee.TrusteeType = TRUSTEE_IS_WELL_KNOWN_GROUP;
+	ea.Trustee.ptstrName = (LPTSTR)everyoneSID;
+
+	// create SID for app containers
+	PSID allAppsSID = NULL;
+	SID_IDENTIFIER_AUTHORITY appPackageAuthority = SECURITY_APP_PACKAGE_AUTHORITY;
+	AllocateAndInitializeSid(&appPackageAuthority,
+		SECURITY_BUILTIN_APP_PACKAGE_RID_COUNT,
+		SECURITY_APP_PACKAGE_BASE_RID,
+		SECURITY_BUILTIN_PACKAGE_ANY_PACKAGE,
+		0, 0, 0, 0, 0, 0, &allAppsSID);
+
+	ea = explicit_accesses[1];
+	ea.grfAccessPermissions = GENERIC_ALL;
+	ea.grfAccessMode = SET_ACCESS;
+	ea.grfInheritance = SUB_CONTAINERS_AND_OBJECTS_INHERIT;
+	ea.Trustee.pMultipleTrustee = NULL;
+	ea.Trustee.MultipleTrusteeOperation = NO_MULTIPLE_TRUSTEE;
+	ea.Trustee.TrusteeForm = TRUSTEE_IS_SID;
+	ea.Trustee.TrusteeType = TRUSTEE_IS_GROUP;
+	ea.Trustee.ptstrName = (LPTSTR)allAppsSID;
+
+	// create DACL
+	SetEntriesInAcl(2, explicit_accesses, NULL, &g_acl);
+
+	// security descriptor
+	InitializeSecurityDescriptor(&g_securittyDescriptor, SECURITY_DESCRIPTOR_REVISION);
+
+	// Add the ACL to the security descriptor. 
+	SetSecurityDescriptorDacl(&g_securittyDescriptor, TRUE, g_acl, FALSE);
+
+	g_securityAttributes.nLength = sizeof(SECURITY_ATTRIBUTES);
+	g_securityAttributes.lpSecurityDescriptor = &g_securittyDescriptor;
+	g_securityAttributes.bInheritHandle = FALSE;
+
+	// cleanup
+	FreeSid(everyoneSID);
+	FreeSid(allAppsSID);
+}
+
+static void cleanup() {
+	if (g_acl != nullptr)
+		LocalFree(g_acl);
+}
+
+// References:
+// https://msdn.microsoft.com/en-us/library/windows/desktop/aa365588(v=vs.85).aspx
+HANDLE connect_pipe(const char* app_name) {
+	HANDLE pipe = INVALID_HANDLE_VALUE;
+	char username[UNLEN + 1];
+	DWORD unlen = UNLEN + 1;
+	if (GetUserNameA(username, &unlen)) {
+		// add username to the pipe path so it will not clash with other users' pipes.
+		char pipe_name[MAX_PATH];
+		sprintf(pipe_name, "\\\\.\\pipe\\%s\\%s_pipe", username, app_name);
+		const size_t buffer_size = 1024;
+		// create the pipe
+		pipe = CreateNamedPipeA(pipe_name,
+			PIPE_ACCESS_DUPLEX,
+			PIPE_TYPE_MESSAGE | PIPE_READMODE_MESSAGE | PIPE_WAIT,
+			PIPE_UNLIMITED_INSTANCES,
+			buffer_size,
+			buffer_size,
+			NMPWAIT_USE_DEFAULT_WAIT,
+			&g_securityAttributes);
+
+		if (pipe != INVALID_HANDLE_VALUE) {
+			// try to connect to the named pipe
+			// NOTE: this is a blocking call
+			if (FALSE == ConnectNamedPipe(pipe, NULL)) {
+				// fail to connect the pipe
+				CloseHandle(pipe);
+				pipe = INVALID_HANDLE_VALUE;
+			}
+		}
+	}
+	return pipe;
+}
+
+void close_pipe(HANDLE pipe) {
+	FlushFileBuffers(pipe);
+	DisconnectNamedPipe(pipe);
+	CloseHandle(pipe);
+}
+
+int read_pipe(HANDLE pipe, char* buf, unsigned long len, unsigned long* error) {
+	DWORD read_len = 0;
+	ReadFile(pipe, buf, len, &read_len, NULL);
+	if (error != nullptr)
+		*error = (unsigned long)GetLastError();
+	return (int)read_len;
+}
+
+int write_pipe(HANDLE pipe, const char* data, unsigned long len, unsigned long* error) {
+	DWORD write_len = 0;
+	WriteFile(pipe, data, len, &write_len, NULL);
+	if (error != nullptr)
+		*error = (unsigned long)GetLastError();
+	return (int)write_len;
+}
+
+BOOL APIENTRY DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved) {
+	switch (ul_reason_for_call) {
+	case DLL_PROCESS_ATTACH:
+		::DisableThreadLibraryCalls(hModule); // disable DllMain calls due to new thread creation
+		init();
+		break;
+	case DLL_PROCESS_DETACH:
+		cleanup();
+		break;
+	}
+	return TRUE;
+}
+
+
+} // extern "C"
--- server/server.py
+++ server/server.py
@@ -1,14 +1,17 @@
 #! python3
-from win32api import *
-from win32pipe import *
-from win32security import *
-from win32event import *
-from win32file import *
-from winerror import *
-from win32con import *
 import threading
 import json
 import sys
+import os
+from ctypes import *
+
+# define Win32 error codes for named pipe I/O
+ERROR_MORE_DATA = 234
+ERROR_IO_PENDING = 997
+
+# import libpipe
+dll_path = os.path.join(os.path.dirname(__file__), "libpipe.dll")
+libpipe = CDLL(dll_path)
 
 from input_methods.serviceManager import textServiceMgr
 
@@ -86,12 +89,11 @@ class Client:
         return reply
 
 
-
 class ClientThread(threading.Thread):
     def __init__(self, client):
         threading.Thread.__init__(self)
         self.client = client
-        self.buf = AllocateReadBuffer(512)
+        self.buf = create_string_buffer(512)
 
     def run(self):
         client = self.client
@@ -101,35 +103,51 @@ class ClientThread(threading.Thread):
         running = True
         while running:
             # Read client requests from the pipe.
-            # http://docs.activestate.com/activepython/3.3/pywin32/win32file__ReadFile_meth.html
             try:
                 read_more = True
                 msg = ""
                 while read_more:
-                    (success, data) = ReadFile(pipe, self.buf, None)
-                    data = data.decode("UTF-8")
+                    # read data from the pipe
+                    error = c_ulong(0)
+                    buf_len = c_ulong(512)
+                    read_len = libpipe.read_pipe(pipe, self.buf, buf_len, pointer(error))
+                    error = error.value
+
+                    # convert content in the read buffer to unicode
+                    if read_len > 0:
+                        data = self.buf.raw[:read_len]
+                        data = data.decode("UTF-8")
+                    else:
+                        data = ""
                     # print("data: ", data)
-                    if success == 0: # success
+
+                    if error == 0: # success
                         msg += data
                         read_more = False
-                    elif success == ERROR_MORE_DATA:
-                        msg += data
-                    elif success == ERROR_IO_PENDING:
-                        pass
+                    elif error == ERROR_MORE_DATA:
+                         msg += data
+                    elif error == ERROR_IO_PENDING:
+                         pass
                     else: # the pipe is broken
                         print("broken pipe")
                         running = False
+                        read_more = False
 
-                # Process the incoming message.
-                msg = json.loads(msg) # parse the json input
-                # print("received msg", success, msg)
+                if msg:
+                    # Process the incoming message.
+                    msg = json.loads(msg) # parse the json input
+                    # print("received msg", success, msg)
 
-                server.acquire_lock() # acquire a lock
-                reply = client.handleRequest(msg)
-                server.release_lock() # release the lock
+                    server.acquire_lock() # acquire a lock
+                    reply = client.handleRequest(msg)
+                    server.release_lock() # release the lock
 
-                reply = json.dumps(reply) # convert object to json
-                WriteFile(pipe, bytes(reply, "UTF-8"), None)
+                    if running:
+                        reply = json.dumps(reply) # convert object to json
+
+                        data = bytes(reply, "UTF-8") # convert to UTF-8
+                        data_len = c_ulong(len(data))
+                        libpipe.write_pipe(pipe, data, data_len, None)
             except:
                 import traceback
                 # print callstatck to know where the exceptions is
@@ -138,104 +156,14 @@ class ClientThread(threading.Thread):
                 print("exception!", sys.exc_info())
                 break
 
-        FlushFileBuffers(pipe)
-        DisconnectNamedPipe(pipe)
-        CloseHandle(pipe)
+        libpipe.close_pipe(pipe)
         server.remove_client(client)
 
-def createSecurityAttributes():
-    # http://msdn.microsoft.com/en-us/library/windows/desktop/hh448449(v=vs.85).aspx
-    # define new Win 8 app related constants
-    SECURITY_APP_PACKAGE_AUTHORITY = (0,0,0,0,0,15)
-    SECURITY_APP_PACKAGE_BASE_RID = 0x00000002
-    SECURITY_BUILTIN_APP_PACKAGE_RID_COUNT = 2
-    SECURITY_APP_PACKAGE_RID_COUNT = 8
-    SECURITY_CAPABILITY_BASE_RID = 0x00000003
-    SECURITY_BUILTIN_CAPABILITY_RID_COUNT = 2
-    SECURITY_CAPABILITY_RID_COUNT = 5
-    SECURITY_BUILTIN_PACKAGE_ANY_PACKAGE = 0x00000001
 
-    explicit_accesses = []
-    # Create a well-known SID for the Everyone group.
-    # FIXME: we should limit the access to current user only
-    # See this article for details: https://msdn.microsoft.com/en-us/library/windows/desktop/hh448493(v=vs.85).aspx
-    everyoneSID = SID()
-    SECURITY_WORLD_SID_AUTHORITY = (0,0,0,0,0,1)
-    everyoneSID.Initialize(SECURITY_WORLD_SID_AUTHORITY, 1)
-    everyoneSID.SetSubAuthority(0, SECURITY_WORLD_RID)
-
-    # https://services.land.vic.gov.au/ArcGIS10.1/edESRIArcGIS10_01_01_3143/Python/pywin32/PLATLIB/win32/Demos/security/explicit_entries.py
-    ea = {
-        "AccessPermissions": GENERIC_ALL,
-        "AccessMode": SET_ACCESS,
-        "Inheritance": SUB_CONTAINERS_AND_OBJECTS_INHERIT,
-        "Trustee": {
-            "MultipleTrustee": None,
-            "MultipleTrusteeOperation": 0,
-            "TrusteeForm": TRUSTEE_IS_SID,
-            "TrusteeType": TRUSTEE_IS_WELL_KNOWN_GROUP,
-            "Identifier": everyoneSID
-        }
-    }
-    explicit_accesses.append(ea)
-
-    # create SID for app containers
-    allAppsSID = SID()
-    allAppsSID.Initialize(SECURITY_APP_PACKAGE_AUTHORITY, SECURITY_BUILTIN_APP_PACKAGE_RID_COUNT)
-    allAppsSID.SetSubAuthority(0, SECURITY_APP_PACKAGE_BASE_RID)
-    allAppsSID.SetSubAuthority(1, SECURITY_BUILTIN_PACKAGE_ANY_PACKAGE)
-
-    ea = {
-        "AccessPermissions": GENERIC_ALL,
-        "AccessMode": SET_ACCESS,
-        "Inheritance": SUB_CONTAINERS_AND_OBJECTS_INHERIT,
-        "Trustee": {
-            "MultipleTrustee": None,
-            "MultipleTrusteeOperation": 0,
-            "TrusteeForm": TRUSTEE_IS_SID,
-            "TrusteeType": TRUSTEE_IS_GROUP,
-            "Identifier": allAppsSID
-        }
-    }
-    explicit_accesses.append(ea)
-
-    # create DACL
-    acl = ACL()
-    acl.Initialize()
-    acl.SetEntriesInAcl(explicit_accesses)
-
-    # security descriptor
-    sd = SECURITY_DESCRIPTOR()
-    sd.Initialize()
-    sd.SetDacl(True, acl, False)
-
-    sa = SECURITY_ATTRIBUTES()
-    sa.SECURITY_DESCRIPTOR = sd
-    return sa
-
-# https://msdn.microsoft.com/en-us/library/windows/desktop/aa365588(v=vs.85).aspx
 class Server:
     def __init__(self):
         self.lock = threading.Lock()
         self.clients = []
-
-    # This function creates a pipe instance and connects to the client.
-    def create_pipe(self):
-        username = GetUserName()
-        # add username to the pipe path so it will not clash with other users' pipes.
-        name = "\\\\.\\pipe\\%s\\PIME_pipe" % username
-        buffer_size = 1024
-        # create the pipe
-        sa = createSecurityAttributes()
-        pipe = CreateNamedPipe(name,
-                               PIPE_ACCESS_DUPLEX,
-                               PIPE_TYPE_MESSAGE|PIPE_READMODE_MESSAGE|PIPE_WAIT,
-                               PIPE_UNLIMITED_INSTANCES,
-                               buffer_size,
-                               buffer_size,
-                               NMPWAIT_USE_DEFAULT_WAIT,
-                               sa)
-        return pipe
 
     def acquire_lock(self):
         self.lock.acquire()
@@ -245,21 +173,10 @@ class Server:
 
     def run(self):
         while True:
-            pipe = self.create_pipe()
-            if pipe == INVALID_HANDLE_VALUE:
-                return False
-
-            print("pipe created, wait for client")
-            # Wait for the client to connect; if it succeeds, the function returns a nonzero value.
-            # If the function returns zero, GetLastError returns ERROR_PIPE_CONNECTED.
-            # According to Windows API doc, ConnectNamedPipe() returns non-zero on success.
-            # However, the doc of pywin32 stated that it should return zero instead. :-(
-            connected = (ConnectNamedPipe(pipe, None) == 0)
-            if not connected:
-                connected = (GetLastError() == ERROR_PIPE_CONNECTED)
+            pipe = libpipe.connect_pipe(bytes("PIME", "UTF-8"))
 
             # client connected
-            if connected:
+            if pipe != -1:
                 print("client connected")
                 # create a Client instance for the client
                 client = Client(self, pipe)
@@ -283,7 +200,7 @@ class Server:
 def main():
     server = Server()
     server.run()
-    server.close()
+
 
 if __name__ == "__main__":
     main()

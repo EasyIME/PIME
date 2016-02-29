b6ce8408a82005c92268bb2987b60f821e578b17
timtan <tim.yellow@gmail.com>
2016-02-29 16:44:16 +0800

    add a basic english to english version

--- server/input_methods/liu/ime_liu.py
+++ server/input_methods/liu/ime_liu.py
@@ -17,65 +17,72 @@
 
 from keycodes import * # for VK_XXX constants
 from textService import *
+from cin import Cin
 import os.path
 
 class LiuTextService(TextService):
     def __init__(self, client):
         TextService.__init__(self, client)
-        self.icon_dir = os.path.abspath(os.path.dirname(__file__))
+
+	base_dir = os.path.abspath(os.path.dirname(__file__))
+
+        self.icon_dir = base_path
+
+        input_table_path = os.path.join(base_dir, 'liu.cin')
+        with open(input_table_path) as f:
+            self.cin = Cin(f)
+	
 
     def onActivate(self):
         TextService.onActivate(self)
         # Windows 8 systray IME mode icon
         self.addButton("windows-mode-icon",
             icon = os.path.join(self.icon_dir, "chi.ico"),
-            tooltip = "Test Button!",
+            tooltip = "Liu !",
             commandId = 1
         )
         self.customizeUI(candFontSize = 20, candPerRow = 1)
-        self.setSelKeys("1234567890")
-        # self.setSelKeys("asdfjkl;")
+        
+        if self.cin.getSelection():
+            self.setSelKeys(self.cin.getSelection())
 
     def onDeactivate(self):
         TextService.onDeactivate(self)
         self.removeButton("test-btn")
 
+    # return True，系統會呼叫 onKeyDown() 進一步處理這個按鍵
+    # return False，表示我們不需要這個鍵，系統會原封不動把按鍵傳給應用程式
     def filterKeyDown(self, keyEvent):
+
         if not self.isComposing():
             if keyEvent.keyCode == VK_RETURN or keyEvent.keyCode == VK_BACK:
                 return False
+
+        # 如果按下 Alt，可能是應用程式熱鍵，輸入法不做處理
+        if keyEvent.isKeyDown(VK_MENU):
+            return False
+
+        # 如果按下 Ctrl 鍵
+        if keyEvent.isKeyDown(VK_CONTROL):
+            else: # 否則可能是應用程式熱鍵，輸入法不做處理
+                return False
+
         return True
 
     def onKeyDown(self, keyEvent):
-        candidates = ["喵", "描", "秒", "妙"]
-        # handle candidate list
-        if self.showCandidates:
-            if keyEvent.keyCode == VK_UP or keyEvent.keyCode == VK_ESCAPE:
-                self.setShowCandidates(False)
-            elif keyEvent.keyCode >= ord('1') and keyEvent.keyCode <= ord('4'):
-                i = keyEvent.keyCode - ord('1')
-                cand = candidates[i]
-                i = self.compositionCursor - 1
-                if i < 0:
-                    i = 0
-                s = self.compositionString[0:i] + cand + self.compositionString[i + 1:]
-                self.setCompositionString(s)
-                self.setShowCandidates(False)
-            return True
-        else:
-            if keyEvent.keyCode == VK_DOWN:
-                self.setCandidateList(candidates)
-                self.setShowCandidates(True)
-                return True
+
         # handle normal keyboard input
         if not self.isComposing():
             if keyEvent.keyCode == VK_RETURN or keyEvent.keyCode == VK_BACK:
                 return False
-        if keyEvent.keyCode == VK_RETURN or len(self.compositionString) > 10:
+
+        if keyEvent.keyCode == VK_RETURN or keyEvent.keyCode == VK_SPACE or len(self.compositionString) > 10:
             self.setCommitString(self.compositionString)
             self.setCompositionString("")
+
         elif keyEvent.keyCode == VK_BACK and self.compositionString != "":
             self.setCompositionString(self.compositionString[:-1])
+
         elif keyEvent.keyCode == VK_LEFT:
             i = self.compositionCursor - 1
             if i >= 0:
@@ -85,8 +92,13 @@ class LiuTextService(TextService):
             if i <= len(self.compositionString):
                 self.setCompositionCursor(i)
         else:
-            self.setCompositionString(self.compositionString + "喵")
+
+            char_code = keyEvent.charCode
+            new_character = chr(char_code)
+
+            self.setCompositionString(self.compositionString + new_character)
             self.setCompositionCursor(len(self.compositionString))
+
         return True
 
     def onCommand(self, commandId, commandType):

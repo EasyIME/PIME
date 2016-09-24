54a591c82836741b9a74689a3b2f48f1f37f1535
Ken Luo <ken670128@hotmail.com>
2016-09-24 18:38:55 +0800

    [PIME] Fix issues https://github.com/EasyIME/forum/issues/9#issuecomment-249356319

--- PIME/PIMEClient.cpp
+++ PIME/PIMEClient.cpp
@@ -318,14 +318,19 @@ void Client::updateStatus(Json::Value& msg, Ime::EditSession* session) {
 
 	// show message
 	const auto& showMessageVal = msg["showMessage"];
+	bool endComposition = false;
 	if (showMessageVal.isObject()) {
 		const Json::Value& message = showMessageVal["message"];
 		const Json::Value& duration = showMessageVal["duration"];
 		if (message.isString() && duration.isInt()) {
 			if (!textService_->isComposing()) {
 				textService_->startComposition(session->context());
+				endComposition = true;
 			}
 			textService_->showMessage(session, utf8ToUtf16(message.asCString()), duration.asInt());
+			if (endComposition) {
+				textService_->endComposition(session->context());
+			}
 		}
 	}
 }

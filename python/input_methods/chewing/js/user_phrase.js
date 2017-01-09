ee710d253063b26cff4daccc785d1d9045438455
ottokang <ottokang@gmail.com>
2017-01-09 11:55:43 +0800

    - Add basic user phrase check

--- python/input_methods/chewing/js/user_phrase.js
+++ python/input_methods/chewing/js/user_phrase.js
@@ -1,5 +1,6 @@
 // Load user phrases
 function loadUserPhrases() {
+    $("#add_dialog").dialog("close");
     // Reload effect
     var loading_message = $("<div>", {
         id: "loading_message",
@@ -11,7 +12,7 @@ function loadUserPhrases() {
         },
         text: "載入詞彙中，請稍後..."
     });
-    $("body").LoadingOverlay("show", { color: "rgba(80, 80, 80, 0.8)", fade: [0, 400], custom: loading_message });  
+    $("body").LoadingOverlay("show", { color: "rgba(80, 80, 80, 0.8)", fade: [0, 400], custom: loading_message });
     $("#reload").html("載入中...");
     $("#reload").addClass("ui-state-hover");
 
@@ -52,6 +53,48 @@ function onAddPhrase() {
     var phrase = $("#phrase_input").val();
     var bopomofo = $("#bopomofo_input").val();
 
+    // Check empty
+    if (phrase.length < 1) {
+        alert("請輸入詞彙");
+        $("#phrase_input").select();
+        return;
+    }
+
+    if (bopomofo.length < 1) {
+        alert("請輸入注音");
+        $("#bopomofo_input").select();
+        return;
+    }
+
+    // Check bopomofo and phrase count is equal
+    var bopomofo_array = bopomofo.split(" ");
+    if (bopomofo_array.length != phrase.length && phrase.length > 1 && bopomofo.length > 1) {
+        alert("注音符號跟詞彙字數不符");
+        $("#phrase_input").select();
+        return;
+    }
+
+    // Check phrase is chinese
+    for (var i = 0; i < phrase.length; i++) {
+        if (phrase.charCodeAt(i) < 0x4E00 || phrase.charCodeAt(i) > 0x9FFF) {
+            alert("詞彙錯誤，請輸入中文");
+            $("#phrase_input").prop("selectionStart", i);
+            $("#phrase_input").prop("selectionEnd", i + 1);
+            return;
+        }
+    }
+
+    // Check bopomofo is correct
+    var bopomofo_check_string = "ㄅㄆㄇㄈㄉㄊㄋㄌㄍㄎㄏㄐㄑㄒㄓㄔㄕㄖㄗㄘㄙㄧㄨㄩㄚㄛㄜㄝㄞㄟㄠㄡㄢㄣㄤㄥㄦ ˊˇˋ˙";
+    for (var i = 0; i < bopomofo.length; i++) {
+        if (bopomofo_check_string.indexOf(bopomofo.substring(i, i + 1)) == -1) {
+            alert("注音符號錯誤，請輸入正確的注音");
+            $("#bopomofo_input").prop("selectionStart", i);
+            $("#bopomofo_input").prop("selectionEnd", i + 1);
+            return;
+        }
+    }
+
     // Check phrase has repeated
     var phrase_repeated;
     var phrase_repeated_index;
@@ -66,10 +109,9 @@ function onAddPhrase() {
     if (phrase_repeated == true) {
         var phrase_repeated_item = $("#table_content input[type=checkbox]:eq(" + phrase_repeated_index + ")");
         $('html, body').animate({ scrollTop: phrase_repeated_item.offset().top - 200 }, 200);
-        $("#add_dialog").dialog("close");
         phrase_repeated_item.parent().effect("highlight", { color: '#f2f207' }, 5000);
-        phrase_repeated_item.parent().append("<small>（已重複）</small>");
-        window.setInterval(function () { phrase_repeated_item.parent().children("small").remove(); }, 4000);
+        $("#phrase_input").select();
+        alert("詞彙已經重複，請重新輸入");
         return;
     }
 
@@ -98,6 +140,7 @@ function onAddPhrase() {
 
 // Execute remove phrase
 function onRemovePhrase(delete_phrase) {
+    $("#add_dialog").dialog("close");
     var phrases = [];
     if (delete_phrase.phrase == null) {
         if ($("#table_content input[type=checkbox]:checked").length == 0)
@@ -144,6 +187,9 @@ $(function () {
     // add phrase dialog
     $("#add_dialog").dialog({
         autoOpen: false,
+        resizable: false,
+        dialogClass: "fixed_dialog",
+        width: 500,
         buttons: [
             {
                 text: "確定",

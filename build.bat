68c543f2a371a1714fc8296ac24b86175e86d024
Hong Jen-Yee (PCMan) <pcman.tw@gmail.com>
2026-07-18 19:48:11 -0700

    Update CI configs to use the latest windows images with Visual Studio 2026.

--- build.bat
+++ build.bat
@@ -1,10 +1,10 @@
-cmake . -Bbuild -G "Visual Studio 17 2022" -A Win32 -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+cmake . -Bbuild -G "Visual Studio 18 2026" -A Win32 -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 cmake --build build --config Release
 
-cmake . -Bbuild64 -G "Visual Studio 17 2022"  -A x64 -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+cmake . -Bbuild64 -G "Visual Studio 18 2026"  -A x64 -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 cmake --build build64 --config Release --target PIMETextService
 
-cmake . -Bbuild_arm64 -G "Visual Studio 17 2022"  -A ARM64 -DCMAKE_POLICY_VERSION_MINIMUM=3.5
+cmake . -Bbuild_arm64 -G "Visual Studio 18 2026"  -A ARM64 -DCMAKE_POLICY_VERSION_MINIMUM=3.5
 cmake --build build_arm64 --config Release --target PIMETextService
 
 
